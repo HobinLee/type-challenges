841ac7291f99941d16ac5ec0950ac9aa0a8052fb
hobinlee <hobinlee@woowahan.com>
2022-10-10 16:45:21 +0900

    solve: [EASY-00011] Easy Tuple To Object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,21 @@
-type TupleToObject<T extends readonly any[]> = any
+/**
+ * Union 타입이 아닌 튜플은 [P in T[number]] 로 순회가 가능하다.
+ * 
+ * 그 중, string, number, symbol이 아닌 타입은 순회가 되지 않는다.
+ */
+
+type TupleToObject<T extends readonly any[]> = { [P in T[number]] : P };
+
+/************************************************************/
+
+type A = ['hello', 'a', 3, { member: 'string'}, 5];
+
+/**
+  B = {
+    hello: 'hello';
+    a: 'a';
+    3: 3;
+    5: 5;
+  }
+ */
+type B = TupleToObject<A>;
