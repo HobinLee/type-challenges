1c607ca58964ba4c1087c81bb93d6fd14d324300
hobinlee <hobinlee@woowahan.com>
2022-10-10 16:07:23 +0900

    solve: [EASY-00004] Easy Pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,25 @@
-type MyPick<T, K> = any
+/**
+ * in 키워드는 K(string | number | symbol)에 있는 모든 타입을 순회시킴
+ * K가 'A' | 'B' |  'C' 일 경우, P in K = 'A', 'B', 'C'가 됨
+ */
+
+type MyPick<T, K extends keyof T> = { [P in K]: T[P] };
+
+/***************************************************************************************/
+
+type Animal = {
+  type: string;
+  name: string;
+  age: number;
+}
+
+type MyType<K extends string> = {[P in K]: P extends keyof Animal ? Animal[P]: '없는 타입' }
+
+/*
+  Result = { 
+    age: number;
+    A: '없는타입';
+    C: '없는타입';
+  }
+*/
+type Result = MyType<'A' | 'C' | 'age'>;
